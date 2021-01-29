91522b8d8024cef35592e86cfb4b4bdb8b294d51
David-B-Comer <davec2233@gmail.com>
2021-01-29 18:17:01 -0500

    added http interceptor to app module

--- angular8-helloworld-example-tutorial/src/app/app.module.ts
+++ angular8-helloworld-example-tutorial/src/app/app.module.ts
@@ -1,28 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HelloWorldService } from './service/hello-world.service';
-import { MenuComponent } from './menu/menu.component';
 import { HelloWorldComponent } from './hello-world/hello-world.component';
+import { MenuComponent } from './menu/menu.component';
 import { LoginComponent } from './login/login.component';
+
+import { FormsModule } from '@angular/forms';
+import { LogoutComponent } from './logout/logout.component';
+import { HttpInterceptorService } from './httpInterceptor.service';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 @NgModule({
 declarations: [
 AppComponent,
-MenuComponent,
 HelloWorldComponent,
+MenuComponent,
 LoginComponent,
+LogoutComponent
 ],
 imports: [
 BrowserModule,
 AppRoutingModule,
 HttpClientModule,
+FormsModule
 ],
 providers: [
-HelloWorldService,
+{
+provide: HTTP_INTERCEPTORS,
+useClass: HttpInterceptorService,
+multi: true
+}
 ],
 bootstrap: [AppComponent]
 })
